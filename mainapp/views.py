d3ec5bda1eb109f4ab6c722341be5fd6947bbc5f
Sergey Kravchenko <030121980@mail.ru>
2020-10-10 03:59:00 +0500

    lesson 1 work

---
+++ mainapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def main(request):
+    return render(request" mainapp/index.html")
+
+    def main(request):
+    return render(request" mainapp/products.html")
+
+    def main(request):
+    return render(request" mainapp/contact.html")
